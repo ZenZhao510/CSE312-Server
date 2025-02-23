499c4612e042c897ff8106a9e1842091e074550d
Zenithon Zhao <zenithonzhao510@gmail.com>
2025-02-23 03:49:24 -0500

    Added some more tests to response.py

--- util/response.py
+++ util/response.py
@@ -7,7 +7,7 @@ class Response:
         self.status_msg = "OK"
         self.headers_dict = {}
         self.cookies_dict = {}
-        self.body =b""
+        self.body = b""
         pass
 
     def set_status(self, code, text):
@@ -42,6 +42,11 @@ class Response:
         pass
 
     def bytes(self, data):
+        # json should not be in here if bytes is
+        # just a safeguard against calling bytes or text after json
+        if "Content-Type" in self.headers_dict and self.headers_dict["Content-Type"] == "application/json":
+            self.headers_dict.pop("Content-Type")
+            self.body = b""
         self.body = self.body + data
         return self
         pass
@@ -58,24 +63,70 @@ class Response:
         pass
 
     def to_data(self):
-        self.headers({"X-Content-Type-Options":"nosniff", "Content-Length":str(len(self.body))})
         if ("Content-Type" not in self.headers_dict):
             self.headers({"Content-Type":"text/plain; charset=utf-8"})
+        self.headers({"X-Content-Type-Options":"nosniff", "Content-Length":str(len(self.body))})
         ret = "HTTP/1.1 " + str(self.status_code) + " " + str(self.status_msg) + "\r\n"
         for header in self.headers_dict:
             ret = ret + str(header) + ": " + str(self.headers_dict[header]) + "\r\n"
         for cookie in self.cookies_dict:
             ret = ret + "Set-Cookie: " + str(cookie) + "=" + str(self.cookies_dict[cookie]) + "\r\n"
         ret = ret + "\r\n"
-        return ret.encode() + self.body + b"\r\n"
+        return ret.encode() + self.body
 
 
 def test1():
     res = Response()
     res.text("hello")
-    expected = b'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Length: 5\r\n\r\nhello'
+    expected = b'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\nContent-Length: 5\r\n\r\nhello'
     actual = res.to_data()
+    # print(expected)
+    # print(actual)
+    assert expected == actual
+
+def test2():
+    res = Response()
+    res.text("hi")
+    res.json({"id":1})
+    # json overwrites text/bytes 
+    expected = b'HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nX-Content-Type-Options: nosniff\r\nContent-Length: 9\r\n\r\n{"id": 1}'
+    actual = res.to_data()
+    # print(expected)
+    # print(actual)
+    assert expected == actual
+
+def test3():
+    res = Response()
+    res.json({"id":1})
+    res.bytes(b"55")
+    res.text("apples")
+    expected = b'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\nContent-Length: 8\r\n\r\n55apples'
+    actual = res.to_data()
+    # print(expected)
+    # print(actual)
+    assert expected == actual
+
+def test4():
+    res = Response()
+    expected = b'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\nContent-Length: 0\r\n\r\n'
+    actual = res.to_data()
+    assert expected == actual
+
+def test5():
+    res = Response()
+    res.set_status("404", "Not Found")
+    res.text("Requested resource cannot be found")
+    expected = b'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\nContent-Length: 34\r\n\r\nRequested resource cannot be found'
+    actual = res.to_data()
+    # print(expected)
+    # print(actual)
+    assert expected == actual
 
 
 if __name__ == '__main__':
     test1()
+    test2()
+    test3()
+    test4()
+    test5()
+
