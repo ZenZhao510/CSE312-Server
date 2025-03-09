95d77d7da554ec7e9cb7e7f65ab609b3cc26cc44
Zenithon Zhao <67135900+ZenZhao510@users.noreply.github.com>
2025-03-09 00:18:19 -0500

    Implemented validate password, and added decoding special characters in extract credentials

--- util/auth.py
+++ util/auth.py
@@ -1,5 +1,7 @@
 from util.request import Request
-
+from string import ascii_lowercase
+from string import ascii_uppercase
+from string import ascii_letters
 
 def extract_credentials(request):
     url = request.path;
@@ -15,9 +17,41 @@ def extract_credentials(request):
     user = query_list.split("=",1)[1];
     pwd = query_list.split("=",1)[1];
 
+    # convert % encoded passwords as utf-8 characters
+    special = ['!', '@', '#', '$', '^', '&', '(', ')', '-', '_', '=', '%'];
+    encodings = ['%21', '%40', '%23', '%24', '%5E', '%26', '%28', '%29', '%2D', '%5F', '%3D', '%25'];
+    for x in range(len(special)):
+        pwd.replace(special[x], encodings[x]);
+    # return the parsed credentials as list
     credentials = [user, pwd];
     return credentials;
 
 def validate_password(str):
+    if len(str) < 8:
+        return False;
+
+    # check for upper and lowercase characters and num and special
+    contains_lower = False;
+    contains_upper = False;
+    contains_num = False;
+    contains_special = False;
+
+    digits = {0,1,2,3,4,5,6,7,8,9};
+    special = {'!', '@', '#', '$', '^', '&', '(', ')', '-', '_', '=', '%'};
+
+    for char in str:
+        if contains_lower == False and char in ascii_lowercase:
+            contains_lower = True;
+        if contains_upper == False and char in ascii_uppercase:
+            contains_upper = True;
+        if contains_num == False and char in digits:
+            contains_num = True;
+        if contains_special == False and char in special:
+            contains_special = True;
+        if char not in ascii_letters and char not in digits and char not in special:
+            return False;
+    if not contains_lower or not contains_upper or not contains_num:
+        return False;
+
     pass
 
