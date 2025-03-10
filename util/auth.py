b6f79dffabd03b0656f063a91a9aaa008d8049f0
Zenithon Zhao <67135900+ZenZhao510@users.noreply.github.com>
2025-03-09 21:18:46 -0400

    uncommented code in validate_password that would've checked for non-supported characters

--- util/auth.py
+++ util/auth.py
@@ -1,6 +1,6 @@
 # for some fucking reason autolab doesn't want me importing this, so i guess i have to comment out my tests just to submit thanks autolab
 # oh it's probably because autolab needs util, but running tests locally i can't use util in path
-from request import Request
+# from request import Request
 from string import ascii_lowercase
 from string import ascii_uppercase
 from string import ascii_letters
@@ -52,85 +52,92 @@ def validate_password(str):
         if contains_special == False and char in special:
             # print("Special found: "+char)
             contains_special = True
-        # if char not in ascii_lowercase and char not in ascii_uppercase and char not in digits and char not in special:
-            # return False
+        if char not in ascii_lowercase and char not in ascii_uppercase and char not in digits and char not in special:
+            return False
     if not contains_lower or not contains_upper or not contains_num or not contains_special:
         # print("no characters or num or special: "+str)
         return False
 
     return True
 
-def test1():
-    request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=password')
-    credentials = extract_credentials(request)
-    assert credentials[0] == "Hi"
-    assert credentials[1] == "password"
-
-def test2():
-    request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=%21%40%23%24%5E%26%28%29%2D%5F%3D%25')
-    credentials = extract_credentials(request)
-    assert credentials[0] == "Hi"
-    assert credentials[1] == '!@#$^&()-_=%'
-
-def test3():
-    # this would not be a valid request probably, as percent isn't percent-encoded, but this just means it's left untouched by extract_credentials since %11 isn't a supported special char
-    request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=%21%40%23%24%5E%26%28%29%2D%5F%3D%25%11')
-    credentials = extract_credentials(request)
-    assert credentials[0] == "Hi"
-    assert credentials[1] == "!@#$^&()-_=%%11"
-
-def test4():
-    # test to make sure percent doesn't fuck up 21 by turning it into !
-    request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=%2521')
-    credentials = extract_credentials(request)
-    assert credentials[0] == "Hi"
-    assert credentials[1] == '%21'
-
-
-def test5():
-    # basic test with only lowercase
-    request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=password')
-    credentials = extract_credentials(request)
-    assert validate_password(credentials[1]) is False
-
-def test6():
-    # basic test with only UPPER
-    request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=PASSWORD')
-    credentials = extract_credentials(request)
-    assert validate_password(credentials[1]) is False
-
-def test7():
-    # basic test with only special
-    request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=%21%40%23%24%5E%26%28%29%2D%5F%3D%25%11')
-    credentials = extract_credentials(request)
-    assert validate_password(credentials[1]) is False
-
-def test8():
-    # basic test with only UPPER
-    request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=01234567890')
-    credentials = extract_credentials(request)
-    assert validate_password(credentials[1]) is False
-
-def test9():
-    # basic test with only UPPER
-    request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=0%21aB')
-    credentials = extract_credentials(request)
-    assert validate_password(credentials[1]) is False
-
-def test10():
-    # basic test with only UPPER
-    request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=Password123%21')
-    credentials = extract_credentials(request)
-    assert validate_password(credentials[1]) is True
-
-if __name__ == '__main__':
-    test1()
-    test2()
-    test3()
-    test4()
-    test5()
-    test6()
-    test7()
-    test8()
-    test9()
-    test10()
+# def test1():
+    # request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=password')
+    # credentials = extract_credentials(request)
+    # assert credentials[0] == "Hi"
+    # assert credentials[1] == "password"
+# 
+# def test2():
+    # request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=%21%40%23%24%5E%26%28%29%2D%5F%3D%25')
+    # credentials = extract_credentials(request)
+    # assert credentials[0] == "Hi"
+    # assert credentials[1] == '!@#$^&()-_=%'
+# 
+# def test3():
+    # # this would not be a valid request probably, as percent isn't percent-encoded, but this just means it's left untouched by extract_credentials since %11 isn't a supported special char
+    # request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=%21%40%23%24%5E%26%28%29%2D%5F%3D%25%11')
+    # credentials = extract_credentials(request)
+    # assert credentials[0] == "Hi"
+    # assert credentials[1] == "!@#$^&()-_=%%11"
+# 
+# def test4():
+    # # test to make sure percent doesn't fuck up 21 by turning it into !
+    # request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=%2521')
+    # credentials = extract_credentials(request)
+    # assert credentials[0] == "Hi"
+    # assert credentials[1] == '%21'
+# 
+# 
+# def test5():
+    # # basic test with only lowercase
+    # request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=password')
+    # credentials = extract_credentials(request)
+    # assert validate_password(credentials[1]) is False
+# 
+# def test6():
+    # # basic test with only UPPER
+    # request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=PASSWORD')
+    # credentials = extract_credentials(request)
+    # assert validate_password(credentials[1]) is False
+# 
+# def test7():
+    # # basic test with only special
+    # request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=%21%40%23%24%5E%26%28%29%2D%5F%3D%25%11')
+    # credentials = extract_credentials(request)
+    # assert validate_password(credentials[1]) is False
+# 
+# def test8():
+    # # basic test with only num
+    # request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=01234567890')
+    # credentials = extract_credentials(request)
+    # assert validate_password(credentials[1]) is False
+# 
+# def test9():
+    # # basic test with too short
+    # request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=0%21aB')
+    # credentials = extract_credentials(request)
+    # assert validate_password(credentials[1]) is False
+# 
+# def test10():
+    # # basic test with minimum viable password
+    # request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=Pass123%21')
+    # credentials = extract_credentials(request)
+    # assert validate_password(credentials[1]) is True
+# 
+# def test11():
+    # # basic test with too short
+    # request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\nusername=Hi&password=Password*23%21')
+    # credentials = extract_credentials(request)
+    # assert validate_password(credentials[1]) is False
+# 
+# if __name__ == '__main__':
+    # test1()
+    # test2()
+    # test3()
+    # test4()
+    # test5()
+    # test6()
+    # test7()
+    # test8()
+    # test9()
+    # test10()
+    # test11()
