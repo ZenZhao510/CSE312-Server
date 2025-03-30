fc5c7bbaccc86aa70a61a7a811c7dba596745201
Zenithon Zhao <67135900+ZenZhao510@users.noreply.github.com>
2025-03-30 06:23:57 -0400

    implementation of multipart.py

--- util/multipart.py
+++ util/multipart.py
@@ -13,5 +13,53 @@ from request import Request
 # for each part, parse headers
 # Content-Type, parse the "body" that follows accordingly
 def parse_multipart(request):
+    # create Multipart object
+    multipart = Multipart()
 
-    pass
+    # use Multipart's functions to parse
+    if ("Content-Type" in request.headers):
+        ContentType = request.headers["Content-Type"]
+        # multipart/form-data;boundary=----WebKitFormBoundaryfkz9sCA6fR3CAHN4
+        multipart.setBoundary(ContentType)
+        multipart.addParts(b"\r\n"+request.body)
+    return multipart
+
+class Multipart:
+
+    def __init__(self):
+        self.boundary = ""
+        self.parts = []
+    
+    def setBoundary(self, header):
+        self.boundary = header.split("=")[1]
+        # ----WebKitFormBoundaryfkz9sCA6fR3CAHN4
+
+    # this does not account for body already having removed the first CRLF
+    # first boundary does not have this split, can probably pass a bytearray of CRLF + body for simplicity
+    def addParts(self, body):
+        # this also removes the first empty entry from this split list
+        splitBoundary = body.split(b"\r\n--"+self.boundary+b"\r\n")[1:]
+        for part in splitBoundary:
+            newPart = MultipartPart(part)
+            self.parts.append(newPart)
+
+class MultipartPart:
+    
+    def __init__(self, partData):
+        data = partData.split(b"\r\n\r\n",1)
+        self.content = data[1]
+        headersList = data[0].decode().split("\r\n")
+        self.headers = {}
+        for header in headersList:
+            self.headers[header.split(":",1)[0].strip()] = header.split(":",1)[1].strip()
+        self.name = ""
+        if "Content-Disposition" in self.headers:
+            # split directives in Content-Disposition into a list, then into a dictionary
+            # this accounts for whichever order directives are given
+            directives = {}
+            directivesList = self.headers["Content-Disposition"].split(";")[1:]
+            for directive in directivesList:
+                directives[directive.split("=",1)[0].strip()] = directive.split("=":,1)[1].strip()
+            # assign name field with the proper directive
+            if ("name" in directives):
+                self.name = directives["name"]
